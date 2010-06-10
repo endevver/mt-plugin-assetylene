736bc3acaad67caa66acde9207e8f12a13af5924
Jay Allen <jay@endevver.com>
2010-06-10 15:55:53 -0700

    Initial commit of SVN mirror at http://code.sixapart.com/trac/mtplugins/browser/trunk/Assetylene (currently v1.02)

---
+++ README.txt
@@ -0,0 +1,70 @@
+Assetylene, a plugin for Movable Type
+======================================
+
+This plugin provides a new "Caption" field when inserting an asset into a
+post, and the ability to customize the HTML markup produced for publishing MT
+assets.
+
+Usage
+------
+
+You can create a custom template module, named "Asset Insertion", and that
+template will then be used to create the HTML for any asset you embed
+into a post.
+
+There are a number of template variables made available to this template:
+
+* upload_html: This is the original markup MT constructed for the
+inserted asset.
+* enclose: 1 or 0, depending on whether the asset is to be wrapped with a
+'form' tag.
+* include: 1 or 0, depending on whether the image is to be displayed or
+simply linked to.
+* thumb: 1 or 0, depending on whether the inline image is a thumbnail
+of the original.
+* align: The value of the alignment option ('none', 'left', 'right',
+'center').
+* popup: 1 or 0, depending on whether the thumbnail image is linked
+to a popup of the full image.
+* caption: The value entered for the caption on the insert options dialog.
+* label: The 'label' value of the asset being inserted.
+* description: The 'description' value of the asset being inserted.
+* asset_id: The numeric ID of the asset being inserted.
+* form_tag: The opening HTML form tag MT creates to enclose the
+asset. If you output this, be sure to add a closing form tag
+as well.
+* form_class: The CSS class name(s) applied to the form tag.
+* form_style: The CSS styling applied to the form tag.
+* img_tag: The MT-constructed img tag being inserted (this is unset
+if no image is actually inserted and linked to instead).
+* img_alt: The "alt" attribute of the inserted image.
+* img_height: The numeric height of the inserted image.
+* img_width: The numeric width of the inserted image.
+* img_src: The url of the inserted image.
+* img_style: Any CSS styling applied to the inline image.
+* img_class: Any CSS class name(s) applied to the inline image.
+* a_tag: The "a" tag produced to link the image (if any).
+* a_href: The "href" attribute value of the "a" tag.
+* a_onclick: The "onclick" attribute value of the "a" tag.
+
+Also, the asset itself is in context, so any of the MT Asset tags may be
+used to generate the asset markup. And the current blog is also in context.
+
+An example template can be found in:
+
+    plugins/Assestylene/asset_insertion.mtml
+
+You can use this as a basis for your own custom template.
+
+Installation
+-------------
+
+To install this plugin, drop the files included with this plugin into your
+Movable Type directory (under the mt-static/plugins/Assetylene,
+plugins/Assetylene locations).
+
+License
+--------
+
+This plugin has been released under the terms of the GNU Public License,
+version 2.0.
---
+++ plugins/Assetylene/lib/Assetylene/CMS.pm
@@ -0,0 +1,128 @@
+# This program is distributed under the terms of the
+# GNU General Public License, version 2.
+#
+# $Id: CMS.pm 1534 2009-05-24 23:52:58Z breese $
+
+package Assetylene::CMS;
+
+use strict;
+
+sub asset_options_image {
+    my ($cb, $app, $param, $tmpl) = @_;
+
+    # Assertions:
+    # 'asset_id' template parameter must be present.
+    my $asset_id = $param->{asset_id} or return;
+
+    # Asset object must be loadable
+    my $asset = MT::Asset->load( $asset_id ) or return;
+
+    # The 'image_alignment' MT template node must be in
+    # the template we're working with to add our field above it.
+    my $el = $tmpl->getElementById('image_alignment')
+        or return;
+
+    my $opt = $tmpl->createElement('app:setting', {
+        id => 'image_caption',
+        label => MT->translate('Caption'),
+        label_class => 'no-header',
+        hint => '',
+        show_hint => 0,
+    });
+
+    require MT::Util;
+    # Encode any special characters as HTML entities, since this
+    # description is being placed in an HTML textarea:
+    my $caption_safe = MT::Util::encode_html( $asset->description );
+
+    # Contents of the app:setting tag:
+    $opt->innerHTML(<<HTML);
+    <input type="checkbox" id="insert_caption" name="insert_caption"
+        value="1" />
+    <label for="insert_caption">Insert a caption?</label>
+    <div class="textarea-wrapper"><textarea name="caption" style="height: 36px;" rows="2" cols=""
+        onfocus="getByID('insert_caption').checked=true; return false;"
+        class="full-width">$caption_safe</textarea></div>
+HTML
+    # Insert new field above the 'image_alignment' field:
+    $tmpl->insertBefore($opt, $el);
+    # Force the tokens of the template to be reprocessed now that
+    # we've manipulated it:
+    $tmpl->rescan();
+}
+
+sub asset_insert {
+    my ($cb, $app, $param, $tmpl) = @_;
+
+    my $blog = $app->blog;
+
+    # Assertions:
+    # Load the user-defined "Asset Insertion" template module.
+    # Currently, this template must be named in English. Look both
+    # at the blog and system level for this template.
+    my $insert_tmpl = $app->model('template')->load({
+        name => 'Asset Insertion', type => 'custom',
+        blog_id => [ $blog->id, 0 ] });
+    return unless $insert_tmpl;
+
+    my $asset = $tmpl->context->stash('asset');
+
+    # Collect all the elements of the MT generated asset markup
+    # so they can be manipulated indepdendently by the user-defined
+    # template:
+    my $html = $param->{upload_html};
+    my ($img_tag) = $html =~ /(<img\b[^>]+?>)/s;
+    my ($a_tag) = $html =~ /(<a\b[^>]+?>)/s;
+    my ($form_tag) = $html =~ /(<form[^>]+?>)/s;
+
+    $param->{enclose} = 1 if $form_tag;
+    $param->{include} = 1 if $app->param('include');
+    $param->{thumb} = 1 if $app->param('thumb');
+    ($param->{align}) = $app->param('align') =~ m/(\w+)/;
+    $param->{caption} = $app->param('insert_caption') ? $app->param('caption') : '';
+    $param->{popup} = 1 if $app->param('popup');
+
+    $param->{label} = $asset->label;
+    $param->{description} = $asset->description;
+    $param->{asset_id} = $asset->id;
+
+    $param->{a_tag} = $a_tag;
+    ($param->{a_href}) = $a_tag =~ /\bhref="(.+?)"/s;
+    ($param->{a_onclick}) = $a_tag =~ /\bonclick="(.+?)"/s;
+
+    $param->{form_tag} = $form_tag;
+    ($param->{form_style}) = $form_tag =~ /\bstyle="([^\"]+)"/s;
+    ($param->{form_class}) = $form_tag =~ /\bclass="([^\"]+)"/s;
+
+    $param->{img_tag} = $img_tag;
+    ($param->{img_height}) = $img_tag =~ /\bheight="(\d+)"/;
+    ($param->{img_width}) = $img_tag =~ /\bwidth="(\d+)"/;
+    ($param->{img_src}) = $img_tag =~ /\bsrc="([^\"]+)"/s;
+    ($param->{img_style}) = $img_tag =~ /\bstyle="([^\"]+)"/s;
+    ($param->{img_class}) = $img_tag =~ /\bclass="([^\"]+)"/s;
+    ($param->{img_alt}) = $img_tag =~ /\balt="([^\"]+)"/s;
+
+    $insert_tmpl->param( $param );
+
+    my $ctx = $insert_tmpl->context;
+    $ctx->stash('blog', $blog);
+    $ctx->stash('blog_id', $blog->id);
+    $ctx->stash('local_blog_id', $blog->id);
+    $ctx->stash('asset', $asset);
+
+    # Process the user-defined template:
+    my $new_html = $insert_tmpl->output;
+
+    if (defined($new_html)) {
+        # Replace the MT generated asset markup with the user-defined
+        # markup:
+        $param->{upload_html} = $new_html;
+    }
+    else {
+        # Template build error: die, so this gets logged (we're in a
+        # callback, so it won't be surfaced to the user unfortunately)
+        die "Error from Asset Insertion module: " . $insert_tmpl->errstr;
+    }
+}
+
+1;
