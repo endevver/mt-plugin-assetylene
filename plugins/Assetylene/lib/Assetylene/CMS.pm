51dd8f82f719098daab088a5b538785b0e1724fa
naoaki011 <naoaki@orion.ocn.ne.jp>
2010-09-23 11:53:01 +0900

    Merge mt.Vicuna Simple's AssetInsertion cleanup and Lightbox effect support.

--- plugins/Assetylene/lib/Assetylene/CMS.pm
+++ plugins/Assetylene/lib/Assetylene/CMS.pm
@@ -6,7 +6,9 @@
 package Assetylene::CMS;
 
 use strict;
+use MT 4;
 use Assetylene::L10N;
+use MT::Util qw( encode_html );
 
 sub asset_options_image {
     my ($cb, $app, $param, $tmpl) = @_;
@@ -24,8 +26,8 @@ sub asset_options_image {
         or return;
 
     my $opt = $tmpl->createElement('app:setting', {
-        id => 'image_caption',
-        label => MT->translate('Caption'),
+        id => 'asset_optins',
+        label => MT->translate('Asset Options'),
         label_class => 'no-header',
         hint => '',
         show_hint => 0,
@@ -36,14 +38,65 @@ sub asset_options_image {
     # description is being placed in an HTML textarea:
     my $caption_safe = MT::Util::encode_html( $asset->description );
 
-    # Contents of the app:setting tag:
-    $opt->innerHTML(<<HTML);
-    <input type="checkbox" id="insert_caption" name="insert_caption"
-        value="1" />
-    <label for="insert_caption"><__trans_section component="Assetylene"><__trans phrase='Insert a caption?'></__trans_section></label>
-    <input type="text" name="caption" onfocus="getByID('insert_caption').checked=true; return false;"
-        value="$caption_safe" style="width:16em;" />
+    my $blog_id = $app->param('blog_id');
+    my $plugin = MT->component("Assetylene");
+    my $scope = "blog:".$blog_id;
+
+    use MT::Blog;
+    my $blog = MT::Blog->load($blog_id) or die;
+    my $themeid = $blog->theme_id;
+#    if ($themeid ne 'mtVicunaSimple') {
+
+        my $insert_options = '';
+        my $lb_select1 = $plugin->get_config_value('lb_select1',$scope);
+        my $lightbox_selector1 = MT::Util::encode_html($plugin->get_config_value('lightbox_selector1',$scope),1);
+        if (($lb_select1) && ($lightbox_selector1)) {
+            $insert_options .= '<option value="' . $lightbox_selector1 . '">' . $lightbox_selector1 . '</option>' . "\n";
+        }
+        my $lb_select2 = $plugin->get_config_value('lb_select2',$scope);
+        my $lightbox_selector2 = MT::Util::encode_html($plugin->get_config_value('lightbox_selector2',$scope),1);
+        if (($lb_select2) && ($lightbox_selector2)) {
+            $insert_options .= '<option value="' . $lightbox_selector2 . '">' . $lightbox_selector2 . '</option>' . "\n";
+        }
+        my $lb_select3 = $plugin->get_config_value('lb_select3',$scope);
+        my $lightbox_selector3 = MT::Util::encode_html($plugin->get_config_value('lightbox_selector3',$scope),1);
+        if (($lb_select3) && ($lightbox_selector3)) {
+            $insert_options .= '<option value="' . $lightbox_selector3 . '">' . $lightbox_selector3 . '</option>' . "\n";
+        }
+        my $lb_select4 = $plugin->get_config_value('lb_select4',$scope);
+        my $lightbox_selector4 = MT::Util::encode_html($plugin->get_config_value('lightbox_selector4',$scope),1);
+        if (($lb_select4) && ($lightbox_selector4)) {
+            $insert_options .= '<option value="' . $lightbox_selector4 . '">' . $lightbox_selector4 . '</option>' . "\n";
+        }
+        if ($insert_options eq '') {
+            $insert_options .= '<option value="rel=&quot;lightbox&quot;">rel=&quot;lightbox&quot;</option>' . "\n";
+        }
+
+        # Contents of the app:setting tag:
+        $opt->innerHTML(<<HTML);
+        <div class="field">
+            <input type="checkbox" id="insert_caption" name="insert_caption"
+                value="1" />
+            <label for="insert_caption"><__trans_section component="Assetylene"><__trans phrase='Insert a caption?'></__trans_section></label>
+            <input type="text" name="caption" onfocus="getByID('insert_caption').checked=true; return false;"
+                value="$caption_safe" style="width:16em;" />
+        </div>
+        <div>
+            <input type="checkbox" id="insert_lightbox" name="insert_lightbox"
+                onclick="if(this.checked){document.getElementById('create_thumbnail').checked=true;
+                document.getElementById('thumb_width').focus();
+                }else{
+                document.getElementById('create_thumbnail').checked=false;}"
+                value="1"<mt:if name="make_thumb"> checked="checked" </mt:if> />
+            <label for="insert_lightbox"><__trans_section component="Assetylene"><__trans phrase='Use Lightbox Effect'></__trans_section></label>
+            <select id="insert_class" name="insert_class">
+                $insert_options
+            </select>
+        </div>
 HTML
+
+#    }
+
     # Insert new field above the 'image_alignment' field:
     $tmpl->insertBefore($opt, $el);
     # Force the tokens of the template to be reprocessed now that
@@ -53,6 +106,75 @@ HTML
 
 sub asset_insert {
     my ($cb, $app, $param, $tmpl) = @_;
+
+    my $blog_id = $app->param('blog_id');
+    use MT::Blog;
+    my $blog = MT::Blog->load($blog_id) or die;
+    my $themeid = $blog->theme_id;
+#    if ($themeid ne 'mtVicunaSimple') {
+    doLog('$themeid:'.$themeid);
+
+        my $plugin = MT->component("Assetylene");
+        my $scope = "blog:".$blog_id;
+        my $cleanup_insert = $plugin->get_config_value('cleanup_insert',$scope);
+        doLog('$cleanup_insert:'.$cleanup_insert);
+        my $upload_html = $param->{ upload_html };
+        if ($cleanup_insert) {
+            my $rightalign_class = $plugin->get_config_value('rightalign_class',$scope);
+            my $centeralign_class = $plugin->get_config_value('centeralign_class',$scope);
+            my $leftalign_class = $plugin->get_config_value('leftalign_class',$scope);
+            my $wrap;
+            if ($cleanup_insert == '1' || $cleanup_insert == '') {
+                if ($upload_html =~ / class=\"mt-image-left\"/) {
+                    $wrap = '<p class="'.$leftalign_class.'">';
+                    if ($cleanup_insert == '') {
+                        $wrap = '<p class="img_L">';
+                    }
+                }
+                if ($upload_html =~ / class=\"mt-image-right\"/) {
+                    $wrap = '<p class="'.$rightalign_class.'">';
+                    if ($cleanup_insert == '') {
+                        $wrap = '<p class="img_R">';
+                    }
+                }
+                if ($upload_html =~ / class=\"mt-image-center\"/) {
+                    if ($centeralign_class) {
+                        $wrap = '<p class="'.$centeralign_class.'">';
+                    } else {
+                        $wrap = '<p>';
+                    }
+                    if ($cleanup_insert == '') {
+                        $wrap = '<p>';
+                    }
+                }
+            }
+            my $insert_class = $app->param('insert_class');
+            if ( $app->param('insert_lightbox') ) {
+                $insert_class = '<a '.$insert_class;
+                $upload_html =~ s/<a/$insert_class/g;
+            }
+            if ($cleanup_insert == '2') {
+                $upload_html =~ s/ class=\"mt-image-none\"//i;
+                $rightalign_class = ' class="'.$rightalign_class.'"';
+                $upload_html =~ s/ class=\"mt-image-right\"/$rightalign_class/g;
+                $leftalign_class = ' class="'.$leftalign_class.'"';
+                $upload_html =~ s/ class=\"mt-image-left\"/$leftalign_class/g;
+                if ($centeralign_class) {
+                    $centeralign_class = ' class="'.$centeralign_class.'"';
+                }
+                $upload_html =~ s/ class=\"mt-image-center\"/$centeralign_class/g;
+            } else {
+                $upload_html =~ s/ class=\"mt-image-(none|right|left|center)\"//i;
+            }
+            $upload_html =~ s/ style=\"\"//i;
+            $upload_html =~ s/ style=\"float\: (right|left)\; margin\: 0 (0|20px) 20px (0|20px)\;\"//i;
+            $upload_html =~ s/ style=\"text-align\: center\; display\: block\; margin\: 0 auto 20px\;\"//i;
+            if ($wrap) {
+                $upload_html = $wrap.$upload_html.'</p>';
+            }
+        }
+
+#    }
 
     my $blog = $app->blog;
 
@@ -70,7 +192,7 @@ sub asset_insert {
     # Collect all the elements of the MT generated asset markup
     # so they can be manipulated indepdendently by the user-defined
     # template:
-    my $html = $param->{upload_html};
+    my $html = $upload_html;
     my ($img_tag) = $html =~ /(<img\b[^>]+?>)/s;
     my ($a_tag) = $html =~ /(<a\b[^>]+?>)/s;
     my ($form_tag) = $html =~ /(<form[^>]+?>)/s;
@@ -123,6 +245,15 @@ sub asset_insert {
         # callback, so it won't be surfaced to the user unfortunately)
         die "Error from Asset Insertion module: " . $insert_tmpl->errstr;
     }
+}
+
+sub doLog {
+    my ($msg) = @_; 
+    return unless defined($msg);
+    require MT::Log;
+    my $log = MT::Log->new;
+    $log->message($msg) ;
+    $log->save or die $log->errstr;
 }
 
 1;
--- plugins/Assetylene/lib/Assetylene/L10N/ja.pm
+++ plugins/Assetylene/lib/Assetylene/L10N/ja.pm
@@ -13,8 +13,22 @@ use vars qw( %Lexicon );
 	   => '"Asset Insertion"テンプレートモジュールのサンプルです。',
 	'This module used by Assetylene plugin.'
 	   => 'このモジュールは Assetylene プラグインにより使用されます。',
+	'Sample "Asset Insertion" module for Assetylene. This is a templete module. Not a Template Set.'
+	   => 'Assetylene プラグインにより使用される"Asset Insertion"モジュールのサンプルです。テンプレートモジュールのみです。テンプレートセットではありません。',
+	'AssetInsertion for Assetylene' => 'Assetyleneプラグイン用"Asset Insertion"モジュール',
 	'Insert a caption?' => 'キャプションを挿入',
 	'Asset Insertion' => 'アイテムを挿入する',
+	'Use Lightbox Effect' => 'Lightbox効果を使用する',
+	'CleanUp Asset Insert' => 'アイテム挿入をクリーンアップする',
+	'No CleanUp' => 'クリーンアップしない',
+	'Add Class for Wrap Paragraph tag' => 'pタグで囲み、クラスを追加する',
+	'Add Class for Image tag' => 'imgタグにクラスを追加する',
+	'Alignment Class' => '位置揃えクラス',
+	'Lightbox Selector' => 'Lightboxセレクター',
+	'RightAlign' => '右揃え',
+	'CenterALign' => '中央揃え',
+	'LeftAlign' => '左揃え',
+	'Use' => '使用する',
 );
 
 1;
